423a83d745cf5e4a0bee7a35c287c45636bbaff9
veto14 <42426906+veto14@users.noreply.github.com>
2021-04-22 05:21:56 -0300

    the knight update

---
+++ 1100.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type pos struct {
+	lar int
+	c   int
+	m   int
+}
+
+var list [10000]pos
+
+func main() {
+	var x string
+	var y string
+	fmt.Scan(x)
+	fmt.Scan(y)
+	//completar amanha deu soninho
+}
